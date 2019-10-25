a99958de7268e480635e39b1fee1f8453780b7dc
Adam Berger <adam.g.berger@gmail.com>
2019-10-24 21:58:10 -0400

    Converted app-bar to style-wrapper

--- src/webhook_explorer/containers/app_bar.cljs
+++ src/webhook_explorer/containers/app_bar.cljs
@@ -1,6 +1,5 @@
 (ns webhook-explorer.containers.app-bar
   (:require [reagent.core :as r]
-            ["@material-ui/core/styles" :as styles]
             ["@material-ui/core/AppBar" :default AppBar]
             ["@material-ui/core/Button" :default Button]
             ["@material-ui/core/Toolbar" :default Toolbar]
@@ -9,13 +8,14 @@
             ["@material-ui/core/Typography" :default Typography]
             ["@material-ui/core/Avatar" :default Avatar]
             [webhook-explorer.app-state :as app-state]
+            [webhook-explorer.styles :as styles]
             [webhook-explorer.actions.auth :as auth-actions]))
 
-(def ^:private styles
-  (styles/makeStyles
+(def ^:private styled
+  (styles/style-wrapper
     (fn [theme]
-      (clj->js {:title {:flexGrow 1}
-                :menu-btn {:marginRight (.spacing theme 2)}}))))
+      {:title {:flexGrow 1}
+       :menu-btn {:marginRight (.spacing theme 2)}})))
 
 (defn- avatar []
   (let [{:keys [given-name family-name name pic-url]} (app-state/user-info)]
@@ -24,20 +24,18 @@
       (when (nil? pic-url)
         (str (first given-name) (first family-name)))]))
 
-(defn- -component []
-  (let [s (styles)]
-    (r/as-element
-      [:> AppBar {:position "static"}
-        [:> Toolbar nil
-          [:> IconButton {:edge "start"
-                          :className ""
-                          :color "inherit"
-                          :aria-label "menu"}
-            [:> MenuIcon]]
-          [:> Typography {:variant "h6" :className (.-title s)} "Webhook Explorer"]
-          (if (app-state/logged-in?)
-            [avatar]
-            [:> Button {:color "inherit" :on-click #(auth-actions/sign-in)} "Login"])]])))
+(defn- -component [{:keys [styles]}]
+  [:> AppBar {:position "static"}
+    [:> Toolbar nil
+      [:> IconButton {:edge "start"
+                      :className ""
+                      :color "inherit"
+                      :aria-label "menu"}
+        [:> MenuIcon]]
+      [:> Typography {:variant "h6" :className (.-title styles)} "Webhook Explorer"]
+      (if (app-state/logged-in?)
+        [avatar]
+        [:> Button {:color "inherit" :on-click #(auth-actions/sign-in)} "Login"])]])
 
 (defn component []
-  [:> -component])
+  [styled {} -component])
