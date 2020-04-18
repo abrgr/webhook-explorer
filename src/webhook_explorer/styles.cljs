ade2829672882b26efbb532955937fe5dcc73364
Adam Berger <adam.g.berger@gmail.com>
2020-04-18 14:49:06 -0400

    Finally fixed reagent->react->reagent prop passing

--- src/webhook_explorer/styles.cljs
+++ src/webhook_explorer/styles.cljs
@@ -1,21 +1,7 @@
 (ns webhook-explorer.styles
   (:require [clojure.string :as string]
             [reagent.core :as r]
-            ["@material-ui/core/styles" :as styles]
-            [camel-snake-kebab.core :as csk]
-            [camel-snake-kebab.extras :as cske]))
-
-(defn- kebab-case-props [p]
-  (cske/transform-keys csk/->kebab-case-keyword p))
-
-(defn fix-special-syms [p]
-  "Needed because react camel-casing a->b yields a>b, which isn't reversible"
-  (->> p
-       (mapcat
-        (fn [[k v]]
-          [[k v]
-           [(-> k name (string/replace ">" "->") keyword) v]]))
-       (into {})))
+            ["@material-ui/core/styles" :as styles]))
 
 (defn style-wrapper [class-defs]
   (let [make-styles (styles/makeStyles
@@ -23,18 +9,13 @@
                        (clj->js (if (fn? class-defs)
                                   (class-defs theme)
                                   class-defs))))
-        react-component (fn [js-props]
-                          (let [s (make-styles)
-                                {:keys [wrapped] :as props} (js->clj js-props :keywordize-keys true)]
-                            (r/as-element
-                             [wrapped
-                              (-> props
-                                  (assoc :styles s)
-                                  (dissoc :wrapped)
-                                  fix-special-syms
-                                  kebab-case-props)])))
         wrapper (fn [props wrapped]
-                  [:> react-component (assoc props :wrapped wrapped)])]
+                  (let [react-component
+                        (fn [js-props]
+                          (let [s (make-styles)]
+                            (r/as-element
+                             [wrapped (assoc props :styles s)])))]
+                    [:> react-component props]))]
     wrapper))
 
 (defn inject-css-link [href]
